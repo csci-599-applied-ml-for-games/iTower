00e51294e3421d27dc117ef91e9c6ac1bfb4f5ab
Hao Jin <xxflamingice@163.com>
2020-02-09 21:55:10 -0800

    new project

---
+++ Tower-Defense-Game/Assets/Scripts/Managers/MainMenu.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    [SerializeField] private GameObject mainMenu, optionsMenu;
+
+    public void Options()
+    {
+        if (optionsMenu.activeSelf) // if the options menu is active
+        {
+            mainMenu.SetActive(true);
+            optionsMenu.SetActive(false);
+        }
+        else  // if the options menu is not active
+        {
+            mainMenu.SetActive(false);
+            optionsMenu.SetActive(true);
+        }
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+
+    public void Play()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
